77a86a587e56d5a9a6be732f7fa410f57452c027
zwh <z_wh@sina.cn>
2019-07-11 14:58:40 +0800

    异步函数测试

--- test/hello-test.js
+++ test/hello-test.js
@@ -1,38 +1,38 @@
 const assert = require('assert');
-const sum = require('../hello');
+const fs = require('fs');
+const hello = require('../hello');
 
-describe("测试sum", () => {
-  describe('#sum()', () => {
-    before(function () {
-      console.log('before:');
+describe("#async", () => {
+  describe('#async()', () => {
+    // 如果要测试异步函数，我们要传入的函数需要带一个参数，通常命名为done
+    // 测试异步函数需要在函数内部手动调用done()表示测试成功，done(err)表示测试出错。
+    it('test async function', function (done) {
+      fs.readFile('data.txt', function (err, data) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
     });
 
-    after(function () {
-      console.log('after.');
+    // 对于用ES7的async编写的函数，我们可以这么写
+    it('#async with done', (done) => {
+      (async function () {
+        try {
+          let r = await hello();
+          assert.strictEqual(r, 15);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      })();
     });
 
-    beforeEach(function () {
-      console.log('  beforeEach:');
-    });
-
-    afterEach(function () {
-      console.log('  afterEach.');
-    });
-
-    it('sum() should return 0', () => {
-      assert.strictEqual(sum(), 0);
-    });
-
-    it('sum(1) should return 1', () => {
-      assert.strictEqual(sum(1), 1);
-    });
-
-    it('sum(1, 2) should return 3', () => {
-      assert.strictEqual(sum(1, 2), 3);
-    });
-
-    it('sum(1, 2, 3) should return 6', () => {
-      assert.strictEqual(sum(1, 2, 3), 6);
+    //但是用try...catch太麻烦。还有一种更简单的写法，就是直接把async函数当成同步函数来测试：
+    it('#async function', async () => {
+      let r = await hello();
+      assert.strictEqual(r, 15);
     });
   });
 });

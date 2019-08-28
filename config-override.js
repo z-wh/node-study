4ac68cd3c317f4d68c52ada1c5fc83651d01fb34
zwh <z_wh@sina.cn>
2019-08-28 15:12:04 +0800

    react基本入门练习

--- config-override.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  database: 'production',
-  username: 'www',
-  password: 'secret-password',
-  host: '192.168.1.199',
-};
-
-module.exports = config;
--- init-db.js
+++
@@ -1,6 +0,0 @@
-const model = require('./model.js');
-
-model.sync();
-
-console.log('init db ok.');
-// process.exit(0);

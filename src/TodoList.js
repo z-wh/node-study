7ef8b8b2b1d73740a16150cc92ab95a8ee5b8c67
zwh <z_wh@sina.cn>
2019-09-03 17:19:27 +0800

    完成Redux业务逻辑和UI分离 把UI做为子组件的方式

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import { Input, Button, List } from 'antd';
 import store from './store/index';
 import { inputChangeAction, addItemAction, deleteItemAction } from './store/actionCreators';
-
-// const data = [
-//     '早8点开晨会，分配今天的开发工作',
-//     '早9点和项目经理作开发需求讨论会',
-//     '晚5:30对今日代码进行review'
-// ]
+import TodoListUI from './TodoListUI';
 
 class TodoList extends Component {
 
@@ -19,6 +12,7 @@ class TodoList extends Component {
         this.inputChange = this.inputChange.bind(this);
         this.storeChange = this.storeChange.bind(this);
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
         //订阅者，订阅Redux状态（订阅者模式，一旦发布者发布新消息/更新，订阅者自动触发)
         store.subscribe(this.storeChange);
     }
@@ -46,30 +40,13 @@ class TodoList extends Component {
 
     render() {
         return (
-            <div style = {{ margin: '10px' }}>
-                <div>
-                    <Input
-                        placeholder = {this.state.inputValue}
-                        style = {{ width: '250px', marginRight: '10px' }}
-                        onChange = {this.inputChange}
-                    />
-                    <Button
-                        type ='primary'
-                        onClick = {this.addItem}
-                    >
-                        增加事项
-                    </Button>
-                </div>
-                <div style = {{ width: '300px', margin: '10px' }}>
-                    <List
-                        bordered
-                        dataSource = {this.state.list}
-                        renderItem = {(item,index) => (
-                            <List.Item onClick = {this.deleteItem.bind(this, index)}>{item}</List.Item>
-                        )}
-                    ></List>
-                </div>
-            </div>
+            <TodoListUI
+                inputValue = {this.state.inputValue}
+                list = {this.state.list}
+                inputChange = {this.inputChange}
+                addItem = {this.addItem}
+                deleteItem = {this.deleteItem}
+            />
         );
     }
 }
